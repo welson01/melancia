14943cb6936ab35d6018821b38391bade43f72a7
welson01 <163464283+welson01@users.noreply.github.com>
2024-03-14 17:42:51 -0300

    Add files via upload
    Alterações efetuadas no dia 14 de março de 2024

---
+++ footer.php
@@ -0,0 +1,8 @@
+</div>
+
+    <!-- container que abriga o rodapé da página-->
+    <div class=" container-fluid text-center bg-dark text-light fixed-bottom">
+        <p>Sistema Web desenvovido por Welson Gonçalves</p>
+    </div>
+</body>
+</html>
