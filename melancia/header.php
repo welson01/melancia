9032fe03bdeb375e69e3552b0b95e19dd5a9b413
welson01 <163464283+welson01@users.noreply.github.com>
2024-03-14 17:40:26 -0300

    Add files via upload

---
+++ melancia/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Melancia TB</title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Annapurna+SIL:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <!-- Container que abriga o logotipo da empresa -->
+    <div class="container-fluid text-success text-center mt-3 mb-3">
+        <img src="img/barraca.jpg" width="100">
+        <h1 style="font-family:Annapurna SIL">Melancia TB</h1>
+    </div>
+    
+    <!--- Barra de navegação do sistema --->
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php" title="Ir para a página Inicial">Página inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cadastrar Produto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Container que abriga o conteudo da página -->
+    <div class="container text-center mt-5">
---
+++ melancia/index.php
@@ -0,0 +1,14 @@
+<?php include("header.php") ?>
+
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+        <p>Aqui ficará o conteúdo da página</p>
+    
+<?php include("footer.php") ?>
